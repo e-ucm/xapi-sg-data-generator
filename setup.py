5cbf8e48631300848d2e39e055c3cc997482d3d0
jujusb <jsantilariob@gmail.com>
2020-08-27 19:00:00 +0200

    Addition of the data generator

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='XAPI Serious Game Random Data Generator',
+      version='0.5',
+      description='Random Data Generator following the Experience API for Serious Games Profile (xAPI-SG)',
+      author='eUCM Group, Julio SANTILARIO BERTHILLIER',
+      author_email='',
+      install_requires=['json','datetime','math','copy','ipywidgets', 'IPython','pprint','random'],
+     )
